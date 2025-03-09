f904f0b20607e15af2123a679d2d8f0c63a11c61
raihananhar <raihanannr@gmail.com>
2025-03-10 06:37:33 +0700

    kelima

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 # ALLOWED_HOSTS tanpa batasan
 ALLOWED_HOSTS = ["*"]
--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({"message": "Backend is running!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tracking.urls')),
-    path('', home),  # Tambahin ini buat handle root URL `/`
 ]
